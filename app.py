9d4be82ab7c59435e72ee98542190451e1c32895
Larry Huang <squirrel532@plurk.com>
2019-07-27 00:02:56 +0800

    update

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(
-    name=__name__,
-    static_url='/static',
-    static_folder='./static'
-)
+app = Flask(__name__)
+app.config['CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-@app.route('/', defaults={'path': 'index.html'})
-@app.route('/static/<filename:path>')
-def index(filename):
-    return app.send_static_file(filename)
+
+class Plurk(db.Model):
+    __tablename__ = 'plurks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String())
+
+    def __init__(self, content):
+
+        self.content = content
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+        }
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           background=url_for('static', filename='background.png'))
 
 
 @app.route('/go')
@@ -31,7 +49,7 @@ def data(offset, db):
         return []
 
 
-@app.error(404)
+@app.errorhandler(404)
 def error404(error):
     return '<h1>車速過快 翻車了</h1>'
 
@@ -39,3 +57,7 @@ if os.environ.get('APP_LOCATION') == 'heroku':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
     app.run(host='localhost', port=8080, debug=True)
+
+
+if __name__ == '__main__':
+    app.run()
